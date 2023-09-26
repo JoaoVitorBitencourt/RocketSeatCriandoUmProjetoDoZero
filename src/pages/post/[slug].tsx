6f28d5289cbf11fc5c60081246e010c0abdea580
João Vitor Bitencourt <joaos_vb@hotmail.com>
2023-09-25 21:29:54 -0300

    finalizando alterações

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -4,6 +4,11 @@ import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+import { FaCalendar, FaUser } from "react-icons/fa";
+import { MdOutlineWatchLater } from "react-icons/md";
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import { useRouter } from 'next/router';
 
 interface Post {
   first_publication_date: string | null;
@@ -29,15 +34,51 @@ interface PostProps {
 export default function Post({
   post
 }: PostProps) {
+  const router = useRouter()
+
+  if (router.isFallback) {
+      return <div>Carregando...</div>
+  }
+  const date = new Date(post.first_publication_date).getTime();
+
+  const totalWords = post.data.content.map(el => {
+    return el.body.map(el2 => {
+        return el2.text.split(' ').length;
+    });
+  }).map(el => {
+    return el.reduce((total, content) => {
+        return total += content;
+    })
+  });
+
+  const readTime = Math.ceil(totalWords.reduce((total, content) => {
+    return total += content;
+  }) / 200);
 
   return (
     <div className={styles.container}>
+      <img src={post.data.banner.url} alt="banner" className={styles.Image}/>
       <div className={styles.postContent}>
-        <img src={post.data.banner.url} alt="banner" className={styles.Image}/>
         <h1 className={styles.title}>{post.data.title}</h1>
         <div className={styles.postInfo}>
-          <span>{post.first_publication_date}</span>
-          <span>{post.data.author}</span>
+          <div className={styles.containerInfo}>
+            <FaCalendar className={styles.icon}/>
+            <span>
+            {
+                format(date, "dd MMM yyyy", {
+                  locale: ptBR
+                })
+            }
+            </span>
+          </div>
+          <div className={styles.containerInfo}>
+            <FaUser className={styles.icon}/>
+            <span>{post.data.author}</span>
+          </div>
+          <div className={styles.containerInfo}>
+            <MdOutlineWatchLater />
+            <span>{readTime} min</span>
+          </div>
         </div>
         <div>
           {post.data.content.map((content, index) => (
